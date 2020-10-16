83ae3135e26bf438b10f7285a8e98751bfcc3f01
ovsyannikova <ovsyanoepechenye@yandex.ru>
2020-10-16 17:34:15 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,23 +1,50 @@
+"""
+Файлик конфигурации для тестов проекта Степик.
+"""
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-# Описываем, что ключ language обязателен для запуска данного теста
 def pytest_addoption(parser):
+    # Поддержка выбора браузера из командной  строки
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    # Поддержка параметра языка из командной  строки
     parser.addoption('--language', action='store', default=None,
-                     help="Choose language please")
+                     help="Choose language")
 
-# Описываем, фикстурув которой будем открывать браузер с учетом языка, введенного пользователем
+
 @pytest.fixture(scope="function")
 def browser(request):
-    print("\nstart chrome browser for test..")
-    # Получаем значение параметра language из командной строки
-    user_language = request.config.getoption("language")
-    options = Options()
-    # Запуск браузера с указанным языком пользователя
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
+    # Получаем имя браузера введенное в командной строке
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        # Создаем объект класса опций Хрома
+        options = Options()
+        # Получаем язык введенный в командной строке
+        user_language = request.config.getoption("language")
+        # Передаем в опции введенный язык
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        # Запускаем браузер с опциями
+        browser = webdriver.Chrome(options=options)
+
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        # Создаем объект класса файрфокс профиля
+        fp = webdriver.FirefoxProfile()
+        # Получаем язык введенный в командной строке
+        user_language = request.config.getoption("language")
+        # Передаем в профиль введенный язык
+        fp.set_preference("intl.accept_languages", user_language)
+        # Запускаем браузер с настроенным профилем
+        browser = webdriver.Firefox(firefox_profile=fp)
+
     yield browser
+
     print("\nquit browser..")
     browser.quit()
